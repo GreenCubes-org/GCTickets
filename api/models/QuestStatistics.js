ed05e3898a36875c997ae64152bb9c27ea3d21a3
Arseniy Maximov <localhost@kern0.ru>
2015-06-14 17:26:53 +0300

    add new quests to model

--- api/models/QuestStatistics.js
+++ api/models/QuestStatistics.js
@@ -8,30 +8,36 @@
 
 module.exports = {
 
-	attributes: {
+  attributes: {
 
-		// Count of players
-		players: 'integer',
+    // Count of players
+    players: 'integer',
 
-		// Count of players that finished quest #2
-		quest2: 'integer',
+    // Count of players that finished quest #2
+    quest2: 'integer',
 
-		// Count of players that finished quest #6
-		quest6: 'integer',
+    // Count of players that finished quest #3
+    quest3: 'integer',
 
-		// Count of players that finished quest #11
-		quest11: 'integer',
+    // Count of players that finished quest #4
+    quest4: 'integer',
 
-		// Count of players that finished quest #16
-		quest16: 'integer',
+    // Count of players that finished quest #6
+    quest6: 'integer',
 
-		// Count of players that finished quest #24
-		quest24: 'integer',
+    // Count of players that finished quest #11
+    quest11: 'integer',
 
-		// Date for this info was relevant
-		date: 'date'
-	},
+    // Count of players that finished quest #16
+    quest16: 'integer',
 
-	autoUpdatedAt: false
+    // Count of players that finished quest #24
+    quest24: 'integer',
+
+    // Date for this info was relevant
+    date: 'date'
+  },
+
+  autoUpdatedAt: false
 
 };
